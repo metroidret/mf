6ad625437afceccdb1809970e4b99d4b1eff3071
biosp4rk <37962487+biosp4rk@users.noreply.github.com>
2025-08-20 18:51:23 -0700

    Add config.h

---
+++ include/config.h
@@ -0,0 +1,33 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+/**
+ * @brief Enables debug features, mainly the boot debug menu and the pause debug menu, among other things
+ * 
+ * (see docs/debug.md for more details)
+ * 
+ */
+// #define DEBUG
+
+/**
+ * @brief Indicates that the game is being compiled using a modern compiler
+ * This enables certain features that agbcc lacks
+ * 
+ */
+// #define MODERN
+
+/**
+ * @brief Enables various fixes to known bugs, issues, or bad code.
+ * 
+ * (see docs/bugs_and_glitches.md for reference)
+ * 
+ */
+// #define BUGFIX
+
+/**
+ * @brief Enables the use of actual symbols instead of hardcoded pointers for things in Ewram
+ * 
+ */
+// #define USE_EWRAM_SYMBOLS
+
+#endif /* CONFIG_H*/
