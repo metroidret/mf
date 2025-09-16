534dd994a7129715a91781c39cf94d0321c519e1
JeffGainsNGames <jeffgainsngames@gmail.com>
2025-09-15 20:00:54 -0400

    decompile CheckSetMeltdownEffect
    also add EventBasedEffect

--- src/transparency.c
+++ src/transparency.c
@@ -1,8 +1,25 @@
 #include "transparency.h"
+#include "globals.h"
 
 #include "constants/event.h"
 
 #include "structs/event.h"
+
+/**
+ * @brief 62db0 | 28 | Checks if the meltdown effect should be set
+ * 
+ */
+u8 CheckSetMeltdownEffect(void) {
+    u8 SetMeltdownEffect = FALSE;
+
+    if ((u32) (u8) (gEventCounter - EVENT_ICE_MISSILE_DATA_DOWNLOADED) <= 2) 
+    {
+        SetMeltdownEffect = TRUE;
+        gCurrentEventBasedEffectCopy = EVENT_EFFECT_MELTDOWN;
+    }
+
+    return SetMeltdownEffect;
+}
 
 /**
  * @brief 62dd8 | 18 | Checks if the room should be dark
