e46427a08e0e4858df8a16f80ee72f27e8d40f8a
JRickey <jack.rickey99@gmail.com>
2026-05-23 20:30:38 -0400

    Decompile StatusScreenHandler

--- src/menus/status_screen.c
+++ src/menus/status_screen.c
@@ -26,3 +26,17 @@
  * the corresponding asm/disasm_*.s and verified with
  * tools/agent/compile_and_view_assembly.py before commit.
  */
+
+/**
+ * @brief 7e678 | 38 | Status screen handler
+ *
+ */
+void StatusScreenHandler(void)
+{
+    if (gChangedInput & (KEY_B | KEY_L | KEY_R) && !PAUSE_SCREEN_DATA.pauseDebugEditingValue)
+    {
+        PAUSE_SCREEN_DATA.unk_28 = 0x7;
+        PAUSE_SCREEN_DATA.timer = 0;
+        PAUSE_SCREEN_DATA.pauseDebugEditingValue = FALSE;
+    }
+}
