4ebff069a218faa061d614dcd8fa2868d30bd843
JRickey <jack.rickey99@gmail.com>
2026-05-24 20:18:54 -0400

    Decompile Box2Init

--- src/sprites_AI/box2.c
+++ src/sprites_AI/box2.c
@@ -80,3 +80,68 @@ u8 Box2XMovement(u16 movement, u8 collidedPose)
 
     return collided;
 }
+
+void Box2Init(void)
+{
+    if (gEquipment.beamStatus & BF_WAVE_BEAM)
+    {
+        gCurrentSprite.status = 0;
+        return;
+    }
+
+    gCurrentSprite.samusCollision = SSC_NONE;
+    gCurrentSprite.status |= SPRITE_STATUS_IGNORE_PROJECTILES;
+    gCurrentSprite.drawDistanceTop = BLOCK_TO_PIXEL(1);
+    gCurrentSprite.drawDistanceBottom = BLOCK_TO_PIXEL(1);
+    gCurrentSprite.drawDistanceHorizontal = BLOCK_TO_PIXEL(1);
+    gCurrentSprite.hitboxTop = -BLOCK_TO_SUB_PIXEL(0.625f);
+    gCurrentSprite.hitboxBottom = BLOCK_TO_SUB_PIXEL(1.0f);
+    gCurrentSprite.hitboxLeft = -BLOCK_TO_SUB_PIXEL(0.75f);
+    gCurrentSprite.hitboxRight = BLOCK_TO_SUB_PIXEL(0.75f);
+    gCurrentSprite.work0 = 7;
+    gCurrentSprite.drawOrder = 12;
+    gCurrentSprite.frozenPaletteRowOffset = 2;
+    gCurrentSprite.roomSlot = BOX2_PART_BRAIN;
+    gCurrentSprite.health = GET_PSPRITE_HEALTH(gCurrentSprite.spriteId);
+    gCurrentSprite.properties |= SP_IMMUNE_TO_PROJECTILES;
+    gCurrentSprite.pOam = sBox2Oam_BrainIdle;
+    gCurrentSprite.animationDurationCounter = 0;
+    gCurrentSprite.currentAnimationFrame = 0;
+
+    gSubSpriteData1.yPosition = gCurrentSprite.yPosition;
+    gSubSpriteData1.xPosition = gCurrentSprite.xPosition;
+    gSubSpriteData1.pMultiOam = sBox2MultiSpriteData_Idle;
+    gSubSpriteData1.animationDurationCounter = 0;
+    gSubSpriteData1.currentAnimationFrame = 0;
+    gSubSpriteData1.work0 = 0;
+
+    gCurrentSprite.work2 = 0;
+    gCurrentSprite.status &= ~SPRITE_STATUS_FACING_RIGHT;
+    gCurrentSprite.pose = BOX2_POSE_WAITING_TO_RUN_INIT;
+
+    SpriteSpawnSecondary(SSPRITE_BOX_2_PART, BOX2_PART_FRONT_LEFT_LEG, gCurrentSprite.spritesetGfxSlot,
+        gCurrentSprite.primarySpriteRamSlot, gSubSpriteData1.yPosition, gSubSpriteData1.xPosition, 0);
+    SpriteSpawnSecondary(SSPRITE_BOX_2_PART, BOX2_PART_DEFAULT_1, gCurrentSprite.spritesetGfxSlot,
+        gCurrentSprite.primarySpriteRamSlot, gSubSpriteData1.yPosition, gSubSpriteData1.xPosition, 0);
+    SpriteSpawnSecondary(SSPRITE_BOX_2_PART, BOX2_PART_FRONT_RIGHT_LEG, gCurrentSprite.spritesetGfxSlot,
+        gCurrentSprite.primarySpriteRamSlot, gSubSpriteData1.yPosition, gSubSpriteData1.xPosition, 0);
+    SpriteSpawnSecondary(SSPRITE_BOX_2_PART, BOX2_PART_DEFAULT_3, gCurrentSprite.spritesetGfxSlot,
+        gCurrentSprite.primarySpriteRamSlot, gSubSpriteData1.yPosition, gSubSpriteData1.xPosition, 0);
+    SpriteSpawnSecondary(SSPRITE_BOX_2_PART, BOX2_PART_DEFAULT_4, gCurrentSprite.spritesetGfxSlot,
+        gCurrentSprite.primarySpriteRamSlot, gSubSpriteData1.yPosition, gSubSpriteData1.xPosition, 0);
+    SpriteSpawnSecondary(SSPRITE_BOX_2_PART, BOX2_PART_DEFAULT_5, gCurrentSprite.spritesetGfxSlot,
+        gCurrentSprite.primarySpriteRamSlot, gSubSpriteData1.yPosition, gSubSpriteData1.xPosition, 0);
+    SpriteSpawnSecondary(SSPRITE_BOX_2_PART, BOX2_PART_TOP_SHELL, gCurrentSprite.spritesetGfxSlot,
+        gCurrentSprite.primarySpriteRamSlot, gSubSpriteData1.yPosition, gSubSpriteData1.xPosition, 0);
+    SpriteSpawnSecondary(SSPRITE_BOX_2_PART, BOX2_PART_BOTTOM_SHELL, gCurrentSprite.spritesetGfxSlot,
+        gCurrentSprite.primarySpriteRamSlot, gSubSpriteData1.yPosition, gSubSpriteData1.xPosition, 0);
+    SpriteSpawnSecondary(SSPRITE_BOX_2_PART, BOX2_PART_DEFAULT_9, gCurrentSprite.spritesetGfxSlot,
+        gCurrentSprite.primarySpriteRamSlot, gSubSpriteData1.yPosition, gSubSpriteData1.xPosition, 0);
+    SpriteSpawnSecondary(SSPRITE_BOX_2_PART, BOX2_PART_DEFAULT_10, gCurrentSprite.spritesetGfxSlot,
+        gCurrentSprite.primarySpriteRamSlot, gSubSpriteData1.yPosition, gSubSpriteData1.xPosition, 0);
+    SpriteSpawnSecondary(SSPRITE_BOX_2_PART, BOX2_PART_DEFAULT_11, gCurrentSprite.spritesetGfxSlot,
+        gCurrentSprite.primarySpriteRamSlot, gSubSpriteData1.yPosition, gSubSpriteData1.xPosition, 0);
+
+    SpriteSpawnPrimary(PSPRITE_ELECTRIC_WATER_DAMAGE_BOX_2, gCurrentSprite.roomSlot, 7, 0x10,
+        gCurrentSprite.yPosition, gCurrentSprite.xPosition, 0);
+}
